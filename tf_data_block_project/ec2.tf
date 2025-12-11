85307ebe35b88652182dbf47f15ca13b9b5f2097
pandiacse <pandiacse2025@gmail.com>
2025-12-11 18:47:20 +0530

    add datablock project

---
+++ tf_data_block_project/ec2.tf
@@ -0,0 +1,25 @@
+data "aws_ami" "instance" {
+  most_recent = true
+  owners      = ["self"]
+
+  filter {
+    name   = "name"
+    values = var.ami
+  }
+}
+
+data "aws_subnet" "selected" {
+  id = "subnet-0613b5494200e5fed"
+}
+
+resource "aws_instance" "webserver" {
+  ami           = data.aws_ami.instance.id
+  instance_type = var.machinetype
+  key_name      = "28mercury"
+  subnet_id     = data.aws_subnet.selected.id
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
